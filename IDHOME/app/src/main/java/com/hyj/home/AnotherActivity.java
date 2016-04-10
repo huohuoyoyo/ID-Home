57fdec981c7e3314a647707c75a085ed1867c14d
huohuo <414993500@qq.com>
2016-04-10 17:01:19 +0800

    tianjian

---
+++ IDHOME/app/src/main/java/com/hyj/home/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.hyj.home;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by huoyongjiang on 16/4/10.
+ */
+public class AnotherActivity extends Activity {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
